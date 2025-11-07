f687955e75f2bcca55c093d439668336bdcdae8e
Isabella Lopreti <lopretiisabella@gmail.com>
2025-11-07 10:24:09 -0300

    Modify 'livros' table structure and data
    Added new columns 'paginas', 'categoria', and 'idioma' to the 'livros' table and updated the insert statements accordingly.

--- bibliotecSql.sql
+++ bibliotecSql.sql
@@ -28,14 +28,17 @@ CREATE TABLE IF NOT EXISTS `livros` (
   `capa_url` varchar(255) DEFAULT NULL,
   `publicado_ano` int(11) DEFAULT NULL,
   `criado_em` timestamp NULL DEFAULT current_timestamp(),
+  `paginas` int(11) DEFAULT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `idioma` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Copiando dados para a tabela bibliotec.livros: ~3 rows (aproximadamente)
-INSERT INTO `livros` (`ID`, `titulo`, `autor`, `descricao`, `capa_url`, `publicado_ano`, `criado_em`) VALUES
-	(1, 'Crepúsculo\r\n\r\n', ' Stephenie Meyer', 'Crepúsculo poderia ser uma história comum, não fosse um elemento irresistível: o objeto da paixão da protagonista é um vampiro. Assim, soma-se à paixão um perigo sobrenatural temperado com muito suspense, e o resultado é uma leitura de tirar o fôlego. Um romance repleto das angústias e incertezas da juventude - o arrebatamento, a atração, a ansiedade que antecede cada palavra, cada gesto, e todos os medos.', 'https://m.media-amazon.com/images/I/618fXbK+OkL._SY425_.jpg', 2005, '2025-10-29 13:53:30'),
-	(2, 'Cinquenta Tons de Cinza', 'E. L. James', 'Fifty Shades of Grey é um romance erótico bestseller da autora inglesa Erika Leonard James publicado em 2011. O primeiro livro de uma trilogia que está sendo tratado como o "pornô das mamães" vendeu mais de dez milhões de livros nas seis primeiras semanas.', 'https://m.media-amazon.com/images/I/51XHQHnyciL._SY445_SX342_ML2_.jpg', 2011, '2025-10-29 13:55:18'),
-	(3, 'O fabricante de lágrimas', ' Erin Doom', 'O fenômeno internacional que inspirou o filme da Netflix – um romance proibido entre dois adolescentes que, ao serem adotados pela mesma família, são obrigados a lidar com um amor que pode arruiná-los.', 'https://m.media-amazon.com/images/I/81Vw5NiVLyL._SY425_.jpg', 2023, '2025-10-29 13:57:24');
+INSERT INTO `livros` (`ID`, `titulo`, `autor`, `descricao`, `capa_url`, `publicado_ano`, `criado_em`, `paginas`, `categoria`, `idioma`) VALUES
+	(1, 'Crepúsculo\r\n\r\n', ' Stephenie Meyer', 'Crepúsculo poderia ser uma história comum, não fosse um elemento irresistível: o objeto da paixão da protagonista é um vampiro. Assim, soma-se à paixão um perigo sobrenatural temperado com muito suspense, e o resultado é uma leitura de tirar o fôlego. Um romance repleto das angústias e incertezas da juventude - o arrebatamento, a atração, a ansiedade que antecede cada palavra, cada gesto, e todos os medos.', 'https://m.media-amazon.com/images/I/618fXbK+OkL._SY425_.jpg', 2005, '2025-10-29 16:53:30', 480, 'Romance / Ficção', 'Português'),
+	(2, 'Cinquenta Tons de Cinza', 'E. L. James', 'Fifty Shades of Grey é um romance erótico bestseller da autora inglesa Erika Leonard James publicado em 2011. O primeiro livro de uma trilogia que está sendo tratado como o "pornô das mamães" vendeu mais de dez milhões de livros nas seis primeiras semanas.', 'https://m.media-amazon.com/images/I/51XHQHnyciL._SY445_SX342_ML2_.jpg', 2011, '2025-10-29 16:55:18', 560, 'Romance Adulto', 'Português'),
+	(3, 'O Fabricante de Lágrimas', ' Erin Doom', 'O fenômeno internacional que inspirou o filme da Netflix – um romance proibido entre dois adolescentes que, ao serem adotados pela mesma família, são obrigados a lidar com um amor que pode arruiná-los.', 'https://m.media-amazon.com/images/I/81Vw5NiVLyL._SY425_.jpg', 2023, '2025-10-29 16:57:24', 426, 'Romance / Ficção', 'Português');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
