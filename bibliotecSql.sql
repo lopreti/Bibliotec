7ca3877caa7e03aa76fe9de0669540716b3f8216
Laví <lavizsenai@gmail.com>
2025-12-02 09:39:24 -0300

    Update bibliotecSql.sql with new user structure

--- bibliotecSql.sql
+++ bibliotecSql.sql
@@ -42,9 +42,15 @@ CREATE TABLE IF NOT EXISTS `favoritos` (
   KEY `FK_favoritos_livro` (`livro_id`),
   CONSTRAINT `FK_favoritos_livro` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`livro_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_favoritos_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Copiando dados para a tabela bibliotec.favoritos: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela bibliotec.favoritos: ~5 rows (aproximadamente)
+INSERT INTO `favoritos` (`id`, `usuario_id`, `livro_id`) VALUES
+	(36, 1, 1),
+	(37, 1, 1),
+	(38, 1, 3),
+	(39, 1, 2),
+	(40, 1, 2);
 
 -- Copiando estrutura para tabela bibliotec.livros
 CREATE TABLE IF NOT EXISTS `livros` (
@@ -99,26 +105,29 @@ CREATE TABLE IF NOT EXISTS `reservas` (
   KEY `FK_reservas_livro` (`livro_id`),
   CONSTRAINT `FK_reservas_livro` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`livro_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_reservas_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Copiando dados para a tabela bibliotec.reservas: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela bibliotec.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `senha` varchar(50) NOT NULL,
+  `nome` varchar(250) NOT NULL,
+  `CPF` varchar(11) NOT NULL,
   PRIMARY KEY (`usuario_id`),
   CONSTRAINT `chk_senha` CHECK (char_length(`senha`) >= 8)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Copiando dados para a tabela bibliotec.usuarios: ~1 rows (aproximadamente)
-INSERT INTO `usuarios` (`usuario_id`, `login`, `senha`) VALUES
-	(1, 'isabella', 'senha12345');
+-- Copiando dados para a tabela bibliotec.usuarios: ~3 rows (aproximadamente)
+INSERT INTO `usuarios` (`usuario_id`, `email`, `senha`, `nome`, `CPF`) VALUES
+	(1, 'isabella@gmail.com', 'senha12345', 'isabella lopreti', '01234567890'),
+	(2, 'teste@example.com', 'password123', 'Teste Usuario', '12345678901'),
+	(3, 'lavinia@gmail.com', 'senha54321', 'lavínia chaves', '12345678900');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-
