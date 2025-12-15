40f1eb3bfdd939746ff8d4cb039d7cd568941094
Kelvinskt <Kelvinhenriquepro@gmail.com>
2025-12-14 22:42:46 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- bibliotecSql.sql
+++ bibliotecSql.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           12.0.2-MariaDB - mariadb.org binary distribution
+-- Versão do servidor:           12.1.2-MariaDB - MariaDB Server
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.11.0.7065
+-- HeidiSQL Versão:              12.14.0.7165
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -81,12 +81,12 @@ CREATE TABLE IF NOT EXISTS `livros` (
   `categoria` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`livro_id`),
   CONSTRAINT `chk_quantidade_pagina` CHECK (`quant_paginas` regexp '^[0-9]+$')
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Copiando dados para a tabela bibliotec.livros: ~46 rows (aproximadamente)
 INSERT INTO `livros` (`livro_id`, `titulo`, `autor`, `descricao`, `capa_url`, `publicado_ano`, `criado_em`, `quant_paginas`, `idioma`, `categoria`) VALUES
 	(1, 'Crepúsculo', 'Stephenie Meyer', 'Bella Swan se muda para a pequena e chuvosa cidade de Forks, onde sua vida toma um rumo inesperado ao conhecer Edward Cullen, um misterioso colega de escola. Conforme os dois se aproximam, Bella descobre que Edward pertence a uma família de vampiros e acaba envolvida em um romance proibido que coloca sua vida em risco.', 'https://m.media-amazon.com/images/I/618fXbK+OkL._SY425_.jpg', 2005, '2025-11-19 11:19:42', '288', 'Português', NULL),
-	(2, 'Cinquenta Tons de Cinza', 'E. L. James', 'Quando a estudante Anastasia Steele entrevista o jovem empresário Christian Grey, ela é atraída por sua beleza, poder e mistério. Apesar de suas diferenças, os dois embarcam em um relacionamento intenso e complexo, marcado por regras rígidas, limites pessoais e a busca por confiança e vulnerabilidade.', 'https://m.media-amazon.com/images/I/51XHQHnyciL._SY445_SX342_ML2_.jpg', 2011, '2025-11-19 11:19:42', '480', 'Português', NULL),
+	(2, 'Cinquenta Tons de Cinza', 'E. L. James', 'Quando a estudante Anastasia Steele entrevista o jovem empresário Christian Grey, ela é atraída por sua beleza, poder e mistério. Apesar de suas diferenças, os dois embarcam em um relacionamento intenso e complexo, marcado por regras rígidas, limites pessoais e a busca por confiança e vulnerabilidade.', 'https://m.media-amazon.com/images/I/51XHQHnyciL._SY445_SX342_ML2_.jpg', 2011, '2025-11-19 11:19:42', '480', 'Português', '\r\n'),
 	(3, 'O fabricante de lágrimas', 'Erin Doom', 'Após crescer em um orfanato, Nica finalmente consegue uma chance de recomeçar ao ser adotada. Porém, sua nova vida se complica com a presença de Rigel, um garoto enigmático e distante que também vive na casa. Enquanto Nica tenta entender seus sentimentos e os segredos que cercam Rigel, ela se vê envolvida em uma história de dor, superação e descobertas emocionais.', 'https://m.media-amazon.com/images/I/81Vw5NiVLyL._SY425_.jpg', 2023, '2025-11-19 11:19:42', '560', 'Português', NULL),
 	(24, 'A Culpa é das Estrelas', 'John Green', 'A história de Hazel e Gus, dois adolescentes que se conhecem em um grupo de apoio ao câncer e vivem um romance intenso e emocionante.', 'https://m.media-amazon.com/images/I/41MRMmeNz0L._SY445_SX342_QL70_ML2_.jpg', 2012, '2025-12-12 11:42:38', '288', 'Português', 'Romance'),
 	(25, 'O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'Um piloto cai no deserto e encontra um pequeno príncipe vindo de outro planeta, explorando temas como amizade, amor e perda.', 'https://m.magazineluiza.com.br/a-static/420x420/livro-o-pequeno-principe-antoine-de-saint-exupery/magazineluiza/230998500/64ef6a6aeefdbb47bd715d78ef21ca8e.png', 1943, '2025-12-12 11:42:38', '96', 'Português', 'Infantil'),
@@ -130,19 +130,20 @@ INSERT INTO `livros` (`livro_id`, `titulo`, `autor`, `descricao`, `capa_url`, `p
 	(68, 'As Benevolentes', 'Jonathan Littell', 'Um ex-oficial nazista narra seus próprios crimes e sua visão perturbadora da guerra.', 'https://m.media-amazon.com/images/I/71tmVD7IoCL._UF1000,1000_QL80_.jpg', 2006, '2025-12-12 12:30:34', '992', 'Português', 'Ficção Histórica'),
 	(70, 'A Rainha Vermelha', 'Victoria Aveyard', 'Mare descobre ter poderes extraordinários em um reino dividido entre sangue vermelho e prateado.', 'https://m.media-amazon.com/images/I/81DQMKmWvCL._UF1000,1000_QL80_.jpg', 2015, '2025-12-12 12:30:34', '416', 'Português', 'Fantasia'),
 	(71, 'Sussurro', 'Becca Fitzpatrick', 'Nora conhece Patch, um anjo caído misterioso que a envolve em um romance perigoso.', 'https://m.media-amazon.com/images/I/71vDiJcTPVL._UF1000,1000_QL80_.jpg', 2009, '2025-12-12 12:30:34', '391', 'Português', 'Romance / Fantasia'),
-	(72, 'Sombra e Ossos', 'Leigh Bardugo', 'Alina Starkov descobre possuir um poder raro capaz de mudar o destino de seu mundo, mergulhando em intrigas, magia e conflitos sombrios.', 'https://m.media-amazon.com/images/I/81WKPIFKThL.jpg', 2012, '2025-12-13 22:22:10', '358', 'Português', NULL);
+	(72, 'Sombra e Ossos', 'Leigh Bardugo', 'Alina Starkov descobre possuir um poder raro capaz de mudar o destino de seu mundo, mergulhando em intrigas, magia e conflitos sombrios.', 'https://m.media-amazon.com/images/I/81WKPIFKThL.jpg', 2012, '2025-12-13 22:22:10', '358', 'Português', 'Romance');
 
 -- Copiando estrutura para tabela bibliotec.livros_categorias
 CREATE TABLE IF NOT EXISTS `livros_categorias` (
   `livro_id` int(11) NOT NULL,
   `categoria_id` int(11) NOT NULL,
   PRIMARY KEY (`livro_id`,`categoria_id`),
+  UNIQUE KEY `uk_livro_categoria` (`livro_id`,`categoria_id`),
   KEY `categoria_id` (`categoria_id`),
   CONSTRAINT `livros_categorias_ibfk_1` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`livro_id`) ON DELETE CASCADE,
   CONSTRAINT `livros_categorias_ibfk_2` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Copiando dados para a tabela bibliotec.livros_categorias: ~46 rows (aproximadamente)
+-- Copiando dados para a tabela bibliotec.livros_categorias: ~49 rows (aproximadamente)
 INSERT INTO `livros_categorias` (`livro_id`, `categoria_id`) VALUES
 	(1, 1),
 	(1, 2),
@@ -199,6 +200,7 @@ CREATE TABLE IF NOT EXISTS `reservas` (
   `data_retirada` int(11) DEFAULT NULL,
   `data_devolucao` int(11) DEFAULT NULL,
   `confirmado_email` int(11) DEFAULT NULL,
+  `criado_em` int(11) DEFAULT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'reservado',
   PRIMARY KEY (`id_reservado`),
   KEY `FK_reservas_usuario` (`usuario_id`),
@@ -208,17 +210,17 @@ CREATE TABLE IF NOT EXISTS `reservas` (
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Copiando dados para a tabela bibliotec.reservas: ~10 rows (aproximadamente)
-INSERT INTO `reservas` (`id_reservado`, `usuario_id`, `livro_id`, `data_retirada`, `data_devolucao`, `confirmado_email`, `status`) VALUES
-	(7, 3, 1, NULL, NULL, NULL, 'reservado'),
-	(10, 4, 1, NULL, NULL, NULL, 'reservado'),
-	(11, 4, 29, NULL, NULL, NULL, 'reservado'),
-	(12, 4, 25, NULL, NULL, NULL, 'reservado'),
-	(14, 4, 27, NULL, NULL, NULL, 'reservado'),
-	(15, 4, 30, NULL, NULL, NULL, 'reservado'),
-	(16, 4, 38, NULL, NULL, NULL, 'reservado'),
-	(17, 4, 46, NULL, NULL, NULL, 'reservado'),
-	(18, 4, 58, NULL, NULL, NULL, 'reservado'),
-	(20, 4, 24, NULL, NULL, NULL, 'reservado');
+INSERT INTO `reservas` (`id_reservado`, `usuario_id`, `livro_id`, `data_retirada`, `data_devolucao`, `confirmado_email`, `criado_em`, `status`) VALUES
+	(7, 3, 1, NULL, NULL, NULL, NULL, 'reservado'),
+	(10, 4, 1, NULL, NULL, NULL, NULL, 'reservado'),
+	(11, 4, 29, NULL, NULL, NULL, NULL, 'reservado'),
+	(12, 4, 25, NULL, NULL, NULL, NULL, 'reservado'),
+	(14, 4, 27, NULL, NULL, NULL, NULL, 'reservado'),
+	(15, 4, 30, NULL, NULL, NULL, NULL, 'reservado'),
+	(16, 4, 38, NULL, NULL, NULL, NULL, 'reservado'),
+	(17, 4, 46, NULL, NULL, NULL, NULL, 'reservado'),
+	(18, 4, 58, NULL, NULL, NULL, NULL, 'reservado'),
+	(20, 4, 24, NULL, NULL, NULL, NULL, 'reservado');
 
 -- Copiando estrutura para tabela bibliotec.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -228,19 +230,41 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nome` varchar(250) NOT NULL,
   `CPF` varchar(11) NOT NULL,
   `is_admin` tinyint(1) DEFAULT 0,
-  `telefone` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`usuario_id`),
   CONSTRAINT `chk_senha` CHECK (char_length(`senha`) >= 8)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Copiando dados para a tabela bibliotec.usuarios: ~6 rows (aproximadamente)
-INSERT INTO `usuarios` (`usuario_id`, `email`, `senha`, `nome`, `CPF`, `is_admin`, `telefone`) VALUES
-	(1, 'isabella@gmail.com', '82bc79eb224ea98d47f241bfa30d67c2346232c4c6a6e7be8d3dc69110f904cf', 'isabella lopreti', '01234567890', 0, NULL),
-	(3, 'lavinia@gmail.com', 'f583836223551ee2255d185addae4a54beb4e76818ec1ceaad1122b8b086b540', 'lavínia chaves', '12345678900', 0, '11919895100'),
-	(4, 'lavi@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'laví chaves', '98765432109', 0, NULL),
-	(5, 'lopretis@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'isabella lopreti', '78965412300', 0, NULL),
-	(6, 'admin@bibliotec.com', '201bce2458f00a54130c695ca8d1658319b32206d495adf175847b57bd4a4151', 'Admin Master', '999999999', 1, NULL),
-	(7, 'kelvin@gmal.com', 'kelvinho123', 'kelvin silva', '12345678912', 0, NULL);
+INSERT INTO `usuarios` (`usuario_id`, `email`, `senha`, `nome`, `CPF`, `is_admin`) VALUES
+	(1, 'isabella@gmail.com', '82bc79eb224ea98d47f241bfa30d67c2346232c4c6a6e7be8d3dc69110f904cf', 'isabella lopreti', '01234567890', 0),
+	(3, 'lavinia@gmail.com', 'f583836223551ee2255d185addae4a54beb4e76818ec1ceaad1122b8b086b540', 'lavínia chaves', '12345678900', 0),
+	(4, 'lavi@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'laví chaves', '98765432109', 0),
+	(5, 'lopretis@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'isabella lopreti', '78965412300', 0),
+	(6, 'admin@bibliotec.com', '201bce2458f00a54130c695ca8d1658319b32206d495adf175847b57bd4a4151', 'Admin Master', '999999999', 1),
+	(7, 'kelvin@gmal.com', 'kelvinho123', 'kelvin silva', '12345678912', 0);
+
+-- Copiando estrutura para trigger bibliotec.trg_livro_categoria_ai
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` TRIGGER trg_livro_categoria_ai
+AFTER INSERT ON livros
+FOR EACH ROW
+BEGIN
+    DECLARE v_categoria_id INT;
+
+    SELECT id
+    INTO v_categoria_id
+    FROM categorias
+    WHERE nome = NEW.categoria
+    LIMIT 1;
+
+    IF v_categoria_id IS NOT NULL THEN
+        INSERT INTO livros_categorias (livro_id, categoria_id)
+        VALUES (NEW.livro_id, v_categoria_id);
+    END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
